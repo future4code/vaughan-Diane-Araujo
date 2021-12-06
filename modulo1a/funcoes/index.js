cb8a983646612582d937f506b500ce1dcfed1779
dianesaraujo <dianesaraujo.arq@gmail.com>
2021-12-06 14:28:55 -0300

    funcoes_add

--- modulo1a/funcoes/index.js
+++ modulo1a/funcoes/index.js
@@ -1,4 +1,7 @@
-functin informacaoDoUsuario (){ 
+functin informacaoDoUsuario(){
     let textoSobreUsuario = "Eu sou Diane, tenho 30 anos, moro em Uberlandia e sou estudante."
+
+    console.log(informacaoDoUsuario)
+
+
     
-    console.log (informacaoDoUsuario)
