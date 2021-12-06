1b3cbba1b8f9824f368139749fdb4d6268a9dab9
dianesaraujo <dianesaraujo.arq@gmail.com>
2021-12-06 17:34:09 -0300

    objetos-add

--- modulo1a/funcoes/index.js
+++ modulo1a/funcoes/index.js
@@ -2,6 +2,3 @@ functin informacaoDoUsuario(){
     let textoSobreUsuario = "Eu sou Diane, tenho 30 anos, moro em Uberlandia e sou estudante."
 
     console.log(informacaoDoUsuario)
-
-
-    
---
+++ modulo1a/objetos/index.js
@@ -0,0 +1,47 @@
+// exercicio 1
+// 1.a
+const pessoa = {
+    nome = "diane",
+    apelido = "Ane, Dih, India",
+}
+function imprimir () {pessoa 
+ console.log.pessoa ("Eu sou", nome, "mas pode me chamar de:", apelido)}
+
+
+    // 1.a
+    const novapessoa = {
+... pessoa,
+apelido = ["apelido1", "apelido2", "apelido3"],
+    }
+    function imprimir () {novapessoa
+console.log.novapessoa ("Eu sou", nome, "mas pode me chamar de:", apelido,)}
+
+
+//exercicio 2
+//2a
+const dadospessoas = {
+nome = "Diane",
+idade =  "30", 
+profissao = "estudante"
+}
+
+ const dadospessoa2 = {
+nome= "Ana",
+ idade= 60,
+ profissao= "aposentada" 
+ }
+ console.log.dadosdapessoa (nome, nome.length, idade, profissao, profissao.length)
+ console.log.dadospesso2 (nome, nome.length, idade, profissao, profissao.length)
+
+
+ // ex 3
+
+ const lista = {
+carrinho: () => {},
+frutas = ["manga", "disponibilidade", true, "uva", true, "morango", true],
+ }
+
+ frutas.carrinho.push (lista)
+ console.log (carrinho)
+
+
