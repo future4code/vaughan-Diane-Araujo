615413b4397f782488deaddd02c630fab0bb7dc7
dianesaraujo <dianesaraujo.arq@gmail.com>
2021-12-12 23:27:35 -0300

    add-projeto-black

---
+++ modulo1/projeto-blackjack/desafio.js
@@ -0,0 +1,24 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+    const boasvindas= prompt ("Boas vindas ao jogo de Blackjack! ).toLowerCase()
+
+    if(confirm("Quer iniciar uma nova rodada?")) {
+      // o que fazer se o usuário clicar "ok"
+   } else {
+      // o que fazer se o usuário clicar "cancelar"
+   }
+
+
+
+
+
