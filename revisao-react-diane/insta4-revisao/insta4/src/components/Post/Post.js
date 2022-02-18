a8c9885f86f41b44ada540faf8cbdc31de537a11
dianesaraujo <dianesaraujo.arq@gmail.com>
2022-02-18 17:01:33 -0300

    revisao-react-diane

---
+++ revisao-react-diane/insta4-revisao/insta4/src/components/Post/Post.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    let aoCurtirMuda
+
+    if(this.state.curtido) {
+      aoCurtirMuda = this.state.numeroCurtidas - 1
+    } else {
+      aoCurtirMuda = this.state.numeroCurtidas + 1
+    }
+
+    this.setState({  // altera o estado inicial conforme o click do usuario 
+      curtido: !this.state.curtido,
+      numeroCurtidas: aoCurtirMuda
+    })
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({ // o setState vai alterar o estado inicial a partir do q o usuario for comentando
+      comentando: true, //alterando de false para true
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  }
+}
+
+export default Post
