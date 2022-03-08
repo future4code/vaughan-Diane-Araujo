f41f511b2d3b39b841e9eafa24eae973b2e7914c
dianesaraujo <dianesaraujo.arq@gmail.com>
2022-03-08 08:47:21 -0300

    whats revisao diane

---
+++ revisao-react-diane/WhatsLab-revisao/whatslab-revisao/src/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components'
+import Button from '@material-ui/core/Button';
+
+const AppContainer = styled.div`
+  border: 2px solid black;
+  height: 100vh;
+  box-sizing: border-box;
+  width: 400px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`
+
+const MessagesContainer = styled.div`
+  flex-grow: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+`
+
+const InputsContainer = styled.div`
+  display: flex;
+`
+
+const NameInput = styled.input`
+  width: 100px;
+`
+
+const MessageInput = styled.input`
+  flex-grow: 1;
+`
+
+class App extends React.Component {
+  state = {
+    messages: [],
+    userValue: '',
+    messageValue: ''
+  }
+
+  onChangeUserValue = (event) => {
+    this.setState({userValue: event.target.value})
+  }
+
+  onChangeMessageValue = (event) => {
+    this.setState({messageValue: event.target.value})
+  }
+
+  sendMessage = () => {
+    const newMessage = {
+      user: this.state.userValue,
+      text: this.state.messageValue
+    }
+
+    const newMessagesArray = [newMessage, ...this.state.messages]
+
+    this.setState({messages: newMessagesArray, messageValue: ''})
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MessagesContainer>
+          {this.state.messages.map((message, index) => {
+            return <p key={index}>
+              <strong>{message.user}</strong>: {message.text}
+            </p>
+          })}
+        </MessagesContainer>
+        <InputsContainer>
+          <NameInput
+            onChange={this.onChangeUserValue} 
+            value={this.state.userValue} 
+            placeholder={'Nome'}
+          />
+          <MessageInput
+            onChange={this.onChangeMessageValue} 
+            value={this.state.messageValue} 
+            placeholder={'Mensagem'}
+          />
+          <Button onClick={this.sendMessage}>Enviar</Button>
+        </InputsContainer>
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
+
+
+/* 
+
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.md = new Remarkable();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Olá, **mundo**!' };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    return { __html: this.md.render(this.state.value) };
+  } */
